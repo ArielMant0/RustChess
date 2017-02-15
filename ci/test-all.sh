bc6c6b93baf3282df493e9abeb50234db0228d4e
ArielMant0 <buecher.apps@gmail.com>
2017-02-15 16:38:18 +0100

    Changed Travis Scripts

--- ci/test-all.sh
+++ ci/test-all.sh
@@ -7,29 +7,28 @@ set -o errexit -o nounset
 
 export RUSTFLAGS="--deny warnings"
 
-taskdir=/chess
-
 echo ""
-echo "=== Teste das Projekt in $taskdir"
+echo "=== Teste Commit"
 
-manifest="$taskdir/Cargo.toml"
-if [ -e "$manifest" ]; then
-    echo "=== Cargo-Manifest gefunden in '$manifest' -> Cargo-Modus"
-    cargo test --manifest-path "$manifest"
-elif [ $(ls $taskdir/*.rs | wc -l) -ne 0 ]; then
-    echo "=== Sourcedatei(en) gefunden -> rustc-Modus"
-    for srcfile in $taskdir/*.rs; do
-        echo "=== Kompiliere und teste '$srcfile'..."
-        rustc "$srcfile"
-        rustc --test -o rustctest "$srcfile"
-        ./rustctest
-    done
-else
-    echo ""
-    echo "!!! Falsch konfigurierter Aufgabenordner oder ungelöste Aufgabe"
-    echo "!!! Bitte .rs-Dateien in '$taskdir' ablegen"
-    echo "!!! Oder ein Cargo-Projekt mit 'cargo init' darin erzeugen"
-    echo "!!! Alternativ den Ordner löschen"
-    exit 1
-fi
+manifest="/Cargo.toml"
+
+for directory in *; do
+    if [[ -d $directory ]]; then
+        if [ -e "$directory$manifest" ]; then
+            echo "=== Cargo-Manifest gefunden in '$directory' -> Cargo-Modus"
+            cargo test --manifest-path "$directory$manifest"
+        elif [ $(ls $directory/*.rs | wc -l) -ne 0 ]; then
+            echo "=== Sourcedatei(en) gefunden -> rustc-Modus"
+            for srcfile in $directory/*.rs; do
+                echo "=== Kompiliere und teste '$srcfile'..."
+                rustc "$srcfile"
+                rustc --test -o rustctest "$srcfile"
+                ./rustctest
+            done
+        else
+            echo ""
+            echo "Konnte kein Cargo-Projekt oder Sourcedateien finden"
+            exit 0
+        fi
+    fi
 done
